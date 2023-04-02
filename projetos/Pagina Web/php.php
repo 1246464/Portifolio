e811bf16aafa2ad0476a0668c376846f65e3447c
Maicon Guibson <maiconghibison123@gmail.com>
2023-04-02 16:05:19 -0300

    Revert "Initial commit"
    This reverts commit e89e0834595a02ed6d32602c8e34e99afd85aa17.

--- projetos/Pagina Web/php.php
+++
@@ -1,7 +0,0 @@
-// Aqui vamos buscar um usuário específico no banco de dados e retornar como JSON
-
-$id = $_GET['id'];
-$dsn = "mysql:host=localhost;dbname=meu_banco";
-$user = "meu_usuario";
-$pass = "minha_senha";
-$dbh = new PDO($dsn, $user
